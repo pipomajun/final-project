6c8455cd0ed86b0c8e6fbf1fb1056d7977cad5de
Philipp Ma <philipp.majun@gmail.com>
2022-06-22 15:25:53 +0200

    Add initial setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Final Project</title>
+        <meta name="description" content="UpLeveled final project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Final Project</h1>
+
+        <p>This is going to be my final project.</p>
+      </main>
+    </div>
+  );
+}
