96ad3660dd95ff33973b2e578c8fdde335e1dd84
Philipp Ma <philipp.majun@gmail.com>
2022-07-17 12:03:57 +0200

    Remove unused fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,6 @@ export default function MyDocument() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <link
           href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap"
           rel="stylesheet"
         />
