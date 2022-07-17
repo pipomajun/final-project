2df56d5140ab9be346058ded11fffa47aaa7b42a
Philipp Ma <philipp.majun@gmail.com>
2022-07-17 12:38:46 +0200

    Add preconnenct link to fonts

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,10 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap"
           rel="stylesheet"
         />
       </Head>
