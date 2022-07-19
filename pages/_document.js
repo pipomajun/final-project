2a4c50debdb5a03b17d787c11a433cbe621d9b4a
Philipp Ma <philipp.majun@gmail.com>
2022-07-19 09:21:28 +0200

    Try to fix issue with Google Fonts in deployment

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,11 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap"
           rel="stylesheet"
-          type="text/css"
         />
       </Head>
       <body>
